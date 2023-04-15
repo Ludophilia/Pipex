70adac0800449edadd1c061c43a8a46477f1a37b
Jeffrey Germany <35299787+Ludophilia@users.noreply.github.com>
2023-04-15 18:20:02 +0200

    Kept experimenting on printf redirection. Learnt the importance of using pipe() before fork().

--- experiments/level2/printf/printf_redirect_ex.c
+++ experiments/level2/printf/printf_redirect_ex.c
@@ -6,7 +6,7 @@
 /*   By: jgermany <nyaritakunai@outlook.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/13 21:26:41 by jgermany          #+#    #+#             */
-/*   Updated: 2023/04/15 16:03:21 by jgermany         ###   ########.fr       */
+/*   Updated: 2023/04/15 18:17:33 by jgermany         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,79 +17,70 @@
 
 #define BUFFER_SIZE 50
 
-// Use the pipe, fork, dup, dup2 to redirect printf output to a buffer
-// <child_p> <-------> <parent_p>
-
 int gpt_redirect(void)
 {
-    int fd[2];
-    char buffer[1024];
+    char 	*buffer;
+    int 	fd[2];
 
-    pipe(fd);  // Create a pipe
+	buffer = calloc(BUFFER_SIZE, sizeof(char));
+    pipe(fd);
     if (fork() == 0) {
-        // Child process
-		// printf("Child process:\n");
-        close(fd[0]);  // Close the read end of the pipe
-        dup2(fd[1], STDOUT_FILENO);  // Redirect stdout to the write end of the pipe
-        fprintf(stdout, "Hello, world!\n");  // This will be written to the pipe
-        close(fd[1]);  // Close the write end of the pipe
+        close(fd[0]);
+        printf("[child %i]\n", getpid());
+		// fflush(stdout);
+        dup2(fd[1], STDOUT_FILENO);
+        printf("Hello, world!");
+        close(fd[1]);
     } else {
-        // Parent process
-		// printf("Parent process:\n");
 		// wait(0);
-        close(fd[1]);  // Close the write end of the pipe
-        read(fd[0], buffer, 15);  // Read from the pipe
-        printf("[parent] '%s'\n", buffer);  // This will print "Hello, world!\n"
-        close(fd[0]);  // Close the read end of the pipe
+        close(fd[1]);
+        read(fd[0], buffer, 13);
+        printf("[parent %i] '%s'\n\n", getpid(), buffer);
+        close(fd[0]);
     }
     return 0;
 }
 
-
-int	main(void)
+int	my_redirect(void)
 {
-	int		fds[2];
-	int		stdout_fd;
 	pid_t	pid;
+	int		fds[2];
 	char	*buffer;
 
-	pid = fork();
-	buffer = calloc(BUFFER_SIZE, sizeof(char));
-	if (!buffer || pipe(fds) == -1 || pid == -1)
-		return (1);
-	
-	if (!pid)
+	// pid = fork(); // fork() before pipe() ? So each proc. has its own pipe?
+	// how do they communicate then? Uh? Beware what you fork() with.
+	if (pipe(fds) == -1)
+		return (-1);
+	pid = fork(); // before fork, processes has the same content. 
+	if (pid == -1)
+		return (-1);
+	else if (pid == 0)
 	{
-		// printf("Child: fds[0] = %i, fds[1] = %i\n", fds[0], fds[1]);
-		// close(fds[0]);
-		stdout_fd = dup(STDOUT_FILENO);
+        printf("[child %i]\n", getpid());
+		close(fds[0]); // The child inherits fds from its parents.
 		dup2(fds[1], STDOUT_FILENO);
-		// dprintf(STDOUT_FILENO, "Test\n");
-		// printf("Test\n");
-		write(STDOUT_FILENO, "test", 4);
-		// write(fds[1], "test", 4);
-
-		dup2(stdout_fd, 1);
-		// fprintf(stderr, "[Buffer C] '%s'\n", buffer);
-
-		if (read(fds[0], buffer, 4) == 0)
-			exit(EXIT_FAILURE);
-		printf("[Buffer] '%s'\n", buffer);
-		close(fds[0]);
-		close(fds[1]);
-
-		exit(EXIT_SUCCESS);
+        printf("Hello, world!");
+		close(fds[1]); // That's how piping becomes possible.
+		// exit(EXIT_SUCCESS);
 	}
 	else
 	{
-		wait(0);// I still don't understand how this works
-		// printf("Parent: fds[0] = %i, fds[1] = %i\n", fds[0], fds[1]);
+		// wait(0);
+		buffer = calloc(BUFFER_SIZE, sizeof(char));
+		if (!buffer)
+			return (-1);
 		close(fds[1]);
-		lseek(fds[0], 0, SEEK_SET);
-		read(fds[0], buffer, 4);
-		printf("[Buffer P] '%s'\n", buffer);
+		read(fds[0], buffer, 13);
+        printf("[parent %i - buffer %p] '%s'\n", getpid(), buffer, buffer);
+		free(buffer); // made pipe crash somehow? Depends where buffer is allocd
 		close(fds[0]);
-		exit(EXIT_SUCCESS);
+		// exit(EXIT_SUCCESS);
 	}
-	free(buffer);
+	return (1);
+}
+
+int	main(void)
+{
+	gpt_redirect();
+	my_redirect();
 }
