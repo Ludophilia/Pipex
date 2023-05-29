ccc2c6eafa56677888cbd840b54e791eaa503255
Jeffrey Germany <35299787+Ludophilia@users.noreply.github.com>
2023-05-29 15:51:10 +0200

    Started thinking about a solution

--- commonlibs.h
+++ commonlibs.h
@@ -6,7 +6,7 @@
 /*   By: jgermany <nyaritakunai@outlook.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/13 15:34:34 by jgermany          #+#    #+#             */
-/*   Updated: 2023/05/29 14:42:04 by jgermany         ###   ########.fr       */
+/*   Updated: 2023/05/29 15:50:20 by jgermany         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ typedef struct s_cmd
 	char 	*cmd;
 	int		in[2];
 	int		out[2];
-	// int		mode; // 0 (in > pipe) 1 (pipe > pipe) 2 (pipe > out)
+	// pid_t	pid;
 }	t_cmd;
 
 #endif
